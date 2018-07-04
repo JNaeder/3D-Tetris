ad293c6dc6d3179050fad72c1fc8eeee6bf3720f
JNaeder <j.naeder324@gmail.com>
2018-07-04 17:12:12 -0400

    Init

---
+++ 3D Tetris Test 1/Assets/GameManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static int width = 10;
+    public static int depth = 10;
+    public static int height = 15;
+
+    public static Transform[,,] grid = new Transform[width, height, depth];
+
+
+    public GameObject[] pieces;
+    public Transform spawnPos;
+
+	// Use this for initialization
+	void Start () {
+        SpawnNewPiece();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public bool IsInsideGrid(Vector3 pos) {
+        Vector3 newPos = Round(pos);
+        return ((int)newPos.x >= 0 && (int)newPos.x < width && (int)newPos.z >= 0 && (int)newPos.z < depth && (int)newPos.y >= 0);    
+    }
+
+
+    public Vector3 Round(Vector3 pos) {
+        return new Vector3(Mathf.Round(pos.x), Mathf.Round(pos.y), Mathf.Round(pos.z));
+
+    }
+
+
+    public void SpawnNewPiece() {
+        int randNum = Random.Range(0, pieces.Length);
+
+        Instantiate(pieces[randNum], spawnPos.position, Quaternion.identity);
+
+    }
+
+
+    public void UpdateGrid(Piece piece) {
+        for (int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++){
+                for (int z = 0; z < depth; z++) {
+                    if (grid[x, y, z] != null) {
+                        if (grid[x, y, z].parent == piece.transform) {
+                            grid[x, y, z] = null;
+                        }
+                    }
+
+                }
+        }
+        }
+
+
+        foreach (Transform block in piece.transform) {
+            Vector3 newPos = Round(block.position);
+            if (newPos.y < height)
+            {
+                grid[(int)newPos.x, (int)newPos.y, (int)newPos.z] = block;
+            }
+        }
+
+
+
+    }
+
+
+
+    public void CheckIfAnyRowsAreFull() {
+        for (int y = 0; y < height; y++) {
+            if (IsRowFull(y)) {
+                Debug.Log("Delete Row " + y);
+                DeleteRow(y);
+                MoveAllRowsDown(y);
+                y--;
+            }
+        }
+
+    }
+
+
+    public bool IsRowFull(int y) {
+        for (int x = 0; x < width; x++) {
+            for (int z = 0; z < depth; z++) {
+                if (grid[x, y, z] == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+    void DeleteRow(int y) {
+        for (int x = 0; x < width; x++) {
+            for (int z = 0; z < depth; z++) {
+                Destroy(grid[x, y, z].gameObject);
+                grid[x, y, z] = null;
+            }
+        }
+    }
+
+    void MoveRowDown(int y) {
+        for (int x = 0; x < width; x++) {
+            for (int z = 0; z < depth; z++) {
+                if (grid[x, y, z] != null) {
+                    grid[x, y - 1, z] = grid[x, y, z];
+                    grid[x, y, z] = null;
+                    grid[x, y - 1, z].position += Vector3.down;
+
+                }
+            }
+        }
+
+    }
+
+    void MoveAllRowsDown(int y) {
+        for (int i = y; y < height; y++) {
+            MoveRowDown(y);
+        }
+
+    }
+}
---
+++ 3D Tetris Test 1/Assets/Piece.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour {
+
+    float newSpeed;
+    float speed = 1f;
+
+    GameManager gM;
+
+	// Use this for initialization
+	void Start () {
+        gM = FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();	
+	}
+
+    void Movement() {
+        if (Input.GetKeyDown(KeyCode.D)) {
+            transform.position += Vector3.right;
+            if (IsValidPos())
+            {
+                gM.UpdateGrid(this);
+            }
+            else {
+                transform.position += Vector3.left;
+
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+            if (IsValidPos())
+            {
+                gM.UpdateGrid(this);
+            }
+            else
+            {
+                transform.position += Vector3.right;
+
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.forward;
+            if (IsValidPos())
+            {
+                gM.UpdateGrid(this);
+            }
+            else
+            {
+                transform.position += Vector3.back;
+
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += Vector3.back;
+            if (IsValidPos())
+            {
+                gM.UpdateGrid(this);
+            }
+            else
+            {
+                transform.position += Vector3.forward;
+
+            }
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 90);
+            if (IsValidPos())
+            {
+                gM.UpdateGrid(this);
+            }
+            else
+            {
+                transform.Rotate(0, 0, -90);
+
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 90, 0);
+            if (IsValidPos())
+            {
+                gM.UpdateGrid(this);
+            }
+            else
+            {
+                transform.Rotate(0, -90, 0);
+
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Rotate(90, 0, 0);
+            if (IsValidPos())
+            {
+                gM.UpdateGrid(this);
+            }
+            else
+            {
+                transform.Rotate(-90, 0, 0);
+
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time > newSpeed + speed)
+        {
+            transform.position += Vector3.down;
+            if (IsValidPos())
+            {
+                gM.UpdateGrid(this);
+            }
+            else
+            {
+                gM.CheckIfAnyRowsAreFull();
+                SpawnNextPiece();
+                transform.position += Vector3.up;
+
+            }
+
+
+            newSpeed = Time.time;
+        }
+    }
+
+
+
+
+    bool IsValidPos() {
+        foreach (Transform block in transform) {
+            Vector3 newPos = gM.Round(block.position);
+            if (!gM.IsInsideGrid(block.position)) {
+                return false;
+            }
+            if (newPos.y < GameManager.height)
+            {
+                if (GameManager.grid[(int)newPos.x, (int)newPos.y, (int)newPos.z] != null && GameManager.grid[(int)newPos.x, (int)newPos.y, (int)newPos.z].parent != transform)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+
+    void SpawnNextPiece() {
+        enabled = false;
+        gM.SpawnNewPiece();
+
+    }
+}
